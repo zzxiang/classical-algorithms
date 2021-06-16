b528940b79867b22ef6d3912db2e0e0a364dde8c
朱志翔 <sshu@pasona.tech>
2021-06-17 05:07:29 +0900

    Add comb_mod.py

---
+++ comb_mod.py
@@ -0,0 +1,20 @@
+# 二項係数 nCr の余り（mod p）を高速に計算する、p は素数でないといけない
+# https://blog.satoooh.com/entry/5195/
+def comb(n, r, p):
+    if (r < 0) or (n < r):
+        return 0
+    r = min(r, n - r)
+    return fact[n] * factinv[r] * factinv[n-r] % p
+
+p = 10 ** 9 + 7  # p は素数でないといけない
+N = 10 ** 6  # N は必要分だけ用意する
+fact = [1, 1]  # fact[n] = (n! mod p)
+factinv = [1, 1]  # factinv[n] = ((n!)^(-1) mod p)
+inv = [0, 1]  # factinv 計算用
+ 
+for i in range(2, N + 1):
+    fact.append((fact[-1] * i) % p)
+    inv.append((-inv[p % i] * (p // i)) % p)
+    factinv.append((factinv[-1] * inv[-1]) % p)
+
+print(comb(10, 3, p))
